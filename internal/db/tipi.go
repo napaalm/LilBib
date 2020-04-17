19963c4a73c34e6e6dab52afa3edcbcd32f94209
Virv <casarin.filippo17@gmail.com>
2020-04-17 19:20:04 +0200

    Formattato codice

--- internal/db/tipi.go
+++ internal/db/tipi.go
@@ -5,30 +5,30 @@ import (
 )
 
 type Libro struct {
-	codice uint32
-	titolo string
-	autore uint32
-	genere uint32
+	codice    uint32
+	titolo    string
+	autore    uint32
+	genere    uint32
 	prenotato bool
-	hash string
+	hash      string
 }
 
 type Genere struct {
 	codice uint32
-	nome string
+	nome   string
 }
 
 type Autore struct {
-	codice uint32
-	nome string
+	codice  uint32
+	nome    string
 	cognome string
 }
 
 type Prestito struct {
-	codice uint32
-	libro uint32
-	utente string
+	codice            uint32
+	libro             uint32
+	utente            string
 	data_prenotazione time.Time
-	durata uint32
+	durata            uint32
 	data_restituzione time.Time
 }
