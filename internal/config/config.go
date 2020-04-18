d6953aac39a34dde6a91395e336f60c520acd67f
Filippo Casarin <casarin.filippo17@gmail.com>
2020-04-18 22:26:38 +0200

    Aggiunta commenti

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,28 @@
+/*
+ * config.go
+ *
+ * File per il caricamento e gestione della configurazione
+ *
+ * Copyright (c) 2020 Filippo Casarin <casarin.filippo17@gmail.com>
+ *
+ * This file is part of LilBib.
+ *
+ * LilBib is free software; you can redistribute it and/or modify it
+ * under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or (at
+ * your option) any later version.
+ *
+ * LilBib is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with LilBib; see the file LICENSE. If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+// File per il caricamento e gestione della configurazione
 package config
 
 import (
