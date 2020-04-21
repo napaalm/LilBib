c1d7d5d360defc3e271200fe5ae57ce91289e796
Filippo Casarin <casarin.filippo17@gmail.com>
2020-04-22 00:10:11 +0200

    Improvet hash_test.go

--- internal/hash/hash.go
+++ internal/hash/hash.go
@@ -1,9 +1,9 @@
 package hash
 
 import (
-	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"git.antonionapolitano.eu/napaalm/LilBib/internal/db"
 )
 
@@ -16,25 +16,23 @@ func (e ErrHash) Error() string {
 func Verifica(pass []byte) (db.Libro, error) {
 	codice := uint32(0)
 	for i := uint8(0); i < 4; i++ {
-		codice = uint32(pass[i]) << (i * 8)
+		codice += uint32(pass[i]) << (i * 8)
 	}
 
 	libro, err := db.GetLibro(codice)
-
 	if err != nil {
 		return libro, err
 	}
 
 	hash := sha256.Sum256(pass)
-
-	if !bytes.Equal(hash[:], libro.hash) {
+	if base64.StdEncoding.EncodeToString(hash[:]) != libro.Hashz {
 		return libro, ErrHash{}
 	}
 
 	return libro, nil
 }
 
-func Genera(codice uint32) ([]byte, []byte, error) { // hash password
+func Genera(codice uint32) (string, []byte, error) { // hash password
 	pass := make([]byte, 20)
 
 	for i := uint8(0); codice != 0; i++ {
@@ -43,10 +41,10 @@ func Genera(codice uint32) ([]byte, []byte, error) { // hash password
 	}
 
 	if _, err := rand.Read(pass[4:]); err != nil {
-		return nil, nil, err
+		return "", nil, err
 	}
 
 	hash := sha256.Sum256(pass[:])
 
-	return hash[:], pass, nil
+	return base64.StdEncoding.EncodeToString(hash[:]), pass, nil
 }
--- internal/hash/hash_test.go
+++ internal/hash/hash_test.go
@@ -1,21 +1,44 @@
 package hash
 
 import (
-	"bytes"
+	"fmt"
 	"crypto/sha256"
 	"testing"
+	"encoding/base64"
+	"git.antonionapolitano.eu/napaalm/LilBib/internal/db"
+	"git.antonionapolitano.eu/napaalm/LilBib/internal/config"
 )
 
-func TestGenera(t *testing.T) {
-	hash, pass, err := Genera(0x5428)
+func TestDatabase(t *testing.T) {
+	config.LoadConfig("../../config/config.toml")
 
+	_, err := db.GetLibro(1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestVerifica(t *testing.T) {
+	pass, err := base64.StdEncoding.DecodeString("AQAAAPx/Y5uKUBfNWH8Cijwt55w=")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if len(hash) != 32 {
-		t.Errorf("len(hash): %d\n", len(hash))
+	if _, err = Verifica(pass); err != nil {
+		t.Error(err)
 	}
+}
+
+func TestGenera(t *testing.T) {
+	hash, pass, err := Genera(0x1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// if len(hash) != 32 {
+	// 	t.Errorf("len(hash): %d\n", len(hash))
+	// }
 
 	if len(pass) != 20 {
 		t.Errorf("len(pass): %d\n", len(pass))
@@ -26,8 +49,11 @@ func TestGenera(t *testing.T) {
 		xxx[i] = 0
 	}
 
-	checksum := sha256.Sum256(pass[:])
-	if !bytes.Equal(checksum[:], hash) {
+	checksum := sha256.Sum256(pass)
+	if base64.StdEncoding.EncodeToString(checksum[:]) != hash {
 		t.Errorf("%x != %x\n", checksum, hash)
 	}
+
+	fmt.Println(hash)
+	fmt.Println(base64.StdEncoding.EncodeToString(pass))
 }
