63c43cfd56622fc5ad3e467602ebc3230ab1224e
Filippo Casarin <casarin.filippo17@gmail.com>
2020-04-22 18:14:24 +0200

    Improved hash_test.go

--- internal/hash/hash_test.go
+++ internal/hash/hash_test.go
@@ -1,7 +1,6 @@
 package hash
 
 import (
-	"fmt"
 	"crypto/sha256"
 	"testing"
 	"encoding/base64"
@@ -9,51 +8,52 @@ import (
 	"git.antonionapolitano.eu/napaalm/LilBib/internal/config"
 )
 
-func TestDatabase(t *testing.T) {
+func TestAll(t *testing.T) {
 	config.LoadConfig("../../config/config.toml")
 
-	_, err := db.GetLibro(1)
+	if err := db.InizializzaDB(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	codice, err := db.AddLibro("libro1", 13, 14)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-}
 
-func TestVerifica(t *testing.T) {
-	pass, err := base64.StdEncoding.DecodeString("AQAAAPx/Y5uKUBfNWH8Cijwt55w=")
+	if codice == 0 {
+		t.Error("codice: 0")
+		return
+	}
+
+	hash, pass, err := Genera(codice)
 	if err != nil {
 		t.Error(err)
+		return
 	}
-
-	if _, err = Verifica(pass); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestGenera(t *testing.T) {
-	hash, pass, err := Genera(0x1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// if len(hash) != 32 {
-	// 	t.Errorf("len(hash): %d\n", len(hash))
-	// }
 
 	if len(pass) != 20 {
 		t.Errorf("len(pass): %d\n", len(pass))
-	}
-
-	var xxx [100]int64
-	for i := range xxx {
-		xxx[i] = 0
+		return
 	}
 
 	checksum := sha256.Sum256(pass)
-	if base64.StdEncoding.EncodeToString(checksum[:]) != hash {
-		t.Errorf("%x != %x\n", checksum, hash)
+	if str := base64.StdEncoding.EncodeToString(checksum[:]); str != hash {
+		t.Errorf("%s != %s\n", str, hash)
+		return
 	}
 
-	fmt.Println(hash)
-	fmt.Println(base64.StdEncoding.EncodeToString(pass))
+	db.SetHash(codice, hash)
+
+	libro, err := Verifica(pass)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if libro.Codice != codice {
+		t.Errorf("Il codice non corrisponde: %d != %d", libro.Codice, codice)
+		return
+	}
 }
