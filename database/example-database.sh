0e39406b6fb4b62174ee958681ee34d84ff4ca9d
Antonio Napolitano <nap@antonionapolitano.eu>
2020-06-03 00:23:28 +0200

    Miglioramento output di example-database.sh

--- database/example-database.sh
+++ database/example-database.sh
@@ -7,7 +7,9 @@ docker-compose -f $DIR/docker-compose.yml down
 docker-compose -f $DIR/docker-compose.yml up -d
 
 # import data (hacky hack)
-until docker exec -i database_db_1 sh -c 'exec mysql -uroot -p"secret"' < $DIR/lilbib_example.sql
+printf "Waiting until MariaDB is fully started to create the database structure..."
+until docker exec -i database_db_1 sh -c 'exec mysql -uroot -p"secret"' < $DIR/lilbib_example.sql 2> /dev/null
 do
   sleep 1
 done
+echo " done!"
